9b56dbee8d83e2d8131b357e318d6ba07cc26c39
Wojtek Mazurek <mazurek.wojciech88@gmail.com>
2023-02-21 20:59:19 +0100

    Board

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ const rows = 25;
 const cols = 25;
 let board;
 let context;
+
+
+//Playground
+window.onload = function () {
+  board = document.querySelector(".board");
+  board.height = rows * blockSize;
+  board.width = cols * blockSize;
+  context = board.getContext("2d");
+
+  update();
+}
+
+function update() {
+  context.fillStyle = "black";
+  context.fillRect(0, 0, board.width, board.height);
+}
